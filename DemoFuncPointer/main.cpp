f17e6929ef7becc0fa1d244501e38d60e78b476a
Fang Huai An <liddle.fang@gmail.com>
2016-03-31 11:03:48 +0800

    initial project

---
+++ DemoFuncPointer/main.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+using namespace std;
+
+int doAdd(int a, int b) {
+   return a + b;
+}  
+
+int doMinus(int a, int b) {
+   return a - b;
+} 
+
+int main (int argc, char *argv[])
+{
+	int (*func_ptr)(int, int);
+
+   	//
+	func_ptr = doAdd;
+	printf("function pointer 指向 doAdd => %d\n", (*func_ptr)(5, 3)); 
+
+	//
+	func_ptr = doMinus;
+	printf("function pointer 指向 doMinus => %d\n", (*func_ptr)(5, 3));
+
+
+	return 0;
+}
+
